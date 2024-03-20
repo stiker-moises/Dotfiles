502773c1ff5c2f3d8866d0574cce7c721d918230
Moises Stiker <stiker.moises@gmail.com>
2024-03-20 10:59:30 +0000

    Replace onoff.sh

--- .config/tofi/scripts/onoff.sh
+++ .config/tofi/scripts/onoff.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-RET=$(echo -e "On\nOff" | tofi --prompt-text " " --height 118 --width 140 )
+RET=$(echo -e "On\nOff" | tofi --prompt-text " " --height 94 --width 140 )
 case $RET in
 	On) sh ~/.config/tofi/scripts/on.sh ;;
 	Off) sh ~/.config/tofi/scripts/off.sh ;;
