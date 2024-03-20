5904711c7f0230be948ceb3b61d2a4cb894e7c5c
Moises Stiker <stiker.moises@gmail.com>
2024-03-20 11:04:03 +0000

    Replace power.sh

--- .config/tofi/scripts/power.sh
+++ .config/tofi/scripts/power.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-RET=$(echo -e "Shutdown\nReboot\nLogout" | tofi --prompt-text " " --height 118 --width 140 )
+RET=$(echo -e "Shutdown\nReboot\nLogout" | tofi --prompt-text " " --height 120 --width 130 )
 case $RET in
 	Shutdown) systemctl poweroff ;;
 	Reboot) systemctl reboot ;;
