537a93ad9da21ddefadb599795ce9929698b964f
Moises Stiker <stiker.moises@gmail.com>
2024-03-20 11:06:56 +0000

    Replace off.sh

--- .config/tofi/scripts/off.sh
+++ .config/tofi/scripts/off.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-RET=$(echo -e "Bedroom\nBathroom\nIcecream" | tofi --prompt-text " " --height 118 --width 125 )
+RET=$(echo -e "Bedroom\nBathroom\nIcecream" | tofi --prompt-text " " --height 122 --width 125 )
 case $RET in
 	Bedroom) python ~/.config/tofi/scripts/bedroom-off.py ;;
 	Bathroom) python ~/.config/tofi/scripts/bathroom-off.py ;;
