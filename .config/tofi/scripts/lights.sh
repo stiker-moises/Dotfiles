4e5d9f981e8bd2b6a6890df458047905863493e2
Moises Stiker <stiker.moises@gmail.com>
2024-03-20 02:09:54 +0000

    Upload New File

---
+++ .config/tofi/scripts/lights.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+RET=$(echo -e "Toggle\nDaylight\nNight\nDim" | tofi --prompt-text " " --height 148 --width 140 )
+case $RET in
+	Daylight) python ~/.config/tofi/scripts/broadcast_color.py 41134 0 65535 7000 ;;
+	Night) python ~/.config/tofi/scripts/broadcast_color.py 41134 0 65535 2500 ;;
+	Dim) python ~/.config/tofi/scripts/broadcast_color.py 41134 0 655 2500 ;;
+	Toggle) sh ~/.config/tofi/scripts/onoff.sh ;;
+esac
