075bebfd37d7e1c4d3cd95560d348ea93e3c8778
Moises Stiker <stiker.moises@gmail.com>
2024-03-24 09:50:43 +0000

    Replace dnd.sh

--- .config/waybar/scripts/dnd.sh
+++ .config/waybar/scripts/dnd.sh
@@ -8,6 +8,6 @@ case $1'' in
     makoctl restore
     ;;
 'toggle')
-    makoctl mode | grep 'do-not-disturb' && makoctl mode -r do-not-disturb || makoctl mode -a do-not-disturb
+    makoctl mode | grep 'do-not-disturb' && makoctl mode -r do-not-disturb || makoctl mode -a do-not-disturb ; pkill -RTMIN+11 waybar
     ;;
 esac
