4b28be515523d45bd76b46bb300fbfbeb2ea1aab
Moises Stiker <stiker.moises@gmail.com>
2024-04-24 08:20:35 +0000

    Replace icecream-off.py

--- .config/tofi/scripts/icecream-off.py
+++ .config/tofi/scripts/icecream-off.py
@@ -4,7 +4,6 @@ import sys
 from lifxlan import LifxLAN, Light
 
 def main ():
-    #icecream
     close = Light("d0:73:d5:72:0a:b0", "192.168.1.218")
     close.set_power(0, 0.2)
 if __name__=="__main__":
