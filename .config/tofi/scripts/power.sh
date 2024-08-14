33397ef05e6167d3b8a9174bdbd99c4675fac46e
Moises Stiker <stiker.moises@gmail.com>
2024-08-14 03:57:47 +0000

    Replace power.sh

--- .config/tofi/scripts/power.sh
+++ .config/tofi/scripts/power.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
-RET=$(echo -e "Shutdown\nReboot\nLogout" | tofi --prompt-text " " --height 120 --width 130 )
+RET=$(echo -e "Shutdown\nReboot\nSleep\nLogout" | tofi --prompt-text " " --height 143 --width 125 )
 case $RET in
 	Shutdown) systemctl poweroff ;;
 	Reboot) systemctl reboot ;;
+	Sleep) sleep 0.1 && systemctl suspend & swaylock --screenshot --effect-blur 7x5 --effect-vignette 1:1 ;;
 	Logout) loginctl terminate-user $USER ;;
 esac
