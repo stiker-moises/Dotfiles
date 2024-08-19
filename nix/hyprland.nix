2c854b39c613a2f9b2fbab52ea3cb13c89a7669b
Moises Stiker <stiker.moises@gmail.com>
2024-08-19 22:29:57 +0000

    Replace hyprland.nix

--- nix/hyprland.nix
+++ nix/hyprland.nix
@@ -116,5 +116,16 @@
   security.doas.extraRules = [{
   keepEnv = true;
   persist = true;
-  }];
+}];
+  systemd.services.keyd = {
+    description = "key remapping daemon";
+    enable = true;
+    serviceConfig = {
+      Type = "simple";
+      ExecStart = "${pkgs.keyd}/bin/keyd";
+    };
+    wantedBy = [ "sysinit.target" ];
+    requires = [ "local-fs.target" ];
+    after = [ "local-fs.target" ];
+  };
 }
