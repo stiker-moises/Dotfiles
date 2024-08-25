cb0fd6d700245f3c96505b8f8a479d16090ba446
Moises Stiker <stiker.moises@gmail.com>
2024-08-25 14:02:02 +0000

    Replace anarchy-desktop.nix

--- etc/nixos/anarchy-desktop.nix
+++ etc/nixos/anarchy-desktop.nix
@@ -1,16 +1,16 @@
 { config, pkgs, ... }:
 {
-	boot.initrd.kernelModules = [ "amdgpu" ];
-  	environment.systemPackages = with pkgs; [
-  		telegram-desktop
-  		element-desktop
-		stoken
-		citrix_workspace
-		ungoogled-chromium
-		teams-for-linux
-		spotifywm
-  	];
-	nixpkgs.config.permittedInsecurePackages = [
-        	"jitsi-meet-1.0.8043"
-        ];
+boot.initrd.kernelModules = [ "amdgpu" ];
+environment.systemPackages = with pkgs; [
+	chromium
+	citrix_workspace
+	element-desktop
+	spotifywm
+	stoken
+	teams-for-linux
+	telegram-desktop
+];
+nixpkgs.config.permittedInsecurePackages = [
+	"jitsi-meet-1.0.8043"
+];
 }
