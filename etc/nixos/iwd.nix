f6986b7d885c4745841bfadb1b3d0fd227e24296
Moises Stiker <stiker.moises@gmail.com>
2024-08-31 01:36:54 +0000

    Upload New File

---
+++ etc/nixos/iwd.nix
@@ -0,0 +1,13 @@
+networking = {
+	networkmanager.enable = false;
+	nameservers = [ "1.1.1.1" "9.9.9.9" ];
+	wireless.iwd.settings = {
+		IPv6 = {
+			Enabled = true;
+		};
+		Settings = {
+			AutoConnect = true;
+		};
+	};
+};
+
