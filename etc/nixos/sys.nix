2f1c986ed20a8f0fb55480928e45115f551fdfbe
Moises Stiker <stiker.moises@gmail.com>
2024-08-31 01:39:53 +0000

    Replace sys.nix

--- etc/nixos/sys.nix
+++ etc/nixos/sys.nix
@@ -221,17 +221,5 @@ fileSystems."/" = {
 		"discard"
 	];
 };
-networking = {
-	networkmanager.enable = false;
-	nameservers = [ "1.1.1.1" "9.9.9.9" ];
-	wireless.iwd.settings = {
-		IPv6 = {
-			Enabled = true;
-		};
-		Settings = {
-			AutoConnect = true;
-		};
-	};
-};
 
 }
