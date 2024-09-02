67f2854d57e92366b2ec6ad82c9d17ad4901e555
Moises Stiker <stiker.moises@gmail.com>
2024-09-02 21:32:02 +0000

    Replace power.sh

--- .config/tofi/scripts/power.sh
+++ .config/tofi/scripts/power.sh
@@ -1,8 +1,9 @@
 #!/run/current-system/sw/bin/zsh
-RET=$(echo -e "Shutdown\nReboot\nSleep\nLogout" | tofi --prompt-text " " --height 143 --width 125 )
+RET=$(echo -e "Shutdown\nReboot\nSleep\nHibernate" | tofi --prompt-text " " --height 143 --width 125 )
 case $RET in
 	Shutdown) systemctl poweroff ;;
 	Reboot) systemctl reboot ;;
-	Sleep) sleep 0.1 && systemctl suspend & swaylock --screenshot --effect-blur 7x5 --effect-vignette 1:1 ;;
-	Logout) loginctl terminate-user $USER ;;
+	Sleep) sleep 0.1 && powerprofilesctl set power-saver ; ( systemctl suspend & swaylock --screenshot --effect-blur 7x5 --effect-vignette 1:1 ) && ( rg -qq 0 /sys/class/power_supply/*/online && powerprofilesctl set power-saver || powerprofilesctl set performance ) ;;
+	Hibernate) sleep 0.1 && powerprofilesctl set power-saver ; ( systemctl hibernate & swaylock --screenshot --effect-blur 7x5 --effect-vignette 1:1 ) && ( rg -qq 0 /sys/class/power_supply/*/online && powerprofilesctl set power-saver || powerprofilesctl set performance ) ;;
+#	Logout) loginctl terminate-user $USER ;;
 esac
