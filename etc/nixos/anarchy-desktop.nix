70d3481d3a8b8870ea44a360fbece72a4f2f1411
Moises Stiker <stiker.moises@gmail.com>
2024-09-02 21:51:02 +0000

    Replace anarchy-desktop.nix

--- etc/nixos/anarchy-desktop.nix
+++ etc/nixos/anarchy-desktop.nix
@@ -1,6 +1,5 @@
 { config, pkgs, ... }:
 {
-boot.initrd.kernelModules = [ "amdgpu" ];
 environment.systemPackages = with pkgs; [
 	chromium
 	citrix_workspace
@@ -19,7 +18,7 @@ nixpkgs.config.permittedInsecurePackages = [
 	"jitsi-meet-1.0.8043"
 	"electron-29.4.6"
 ];
-services.udev.extraRules = ''
-  ACTION=="add" SUBSYSTEM=="pci" ATTR{vendor}=="0x1022" ATTR{device}=="0x1484" ATTR{power/wakeup}="disabled"
-'';
+#services.udev.extraRules = ''
+#  ACTION=="add" SUBSYSTEM=="pci" ATTR{vendor}=="0x1022" ATTR{device}=="0x1484" ATTR{power/wakeup}="disabled"
+#'';
 }
