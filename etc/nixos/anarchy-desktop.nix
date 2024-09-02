79e773d568b135774db738e4304180ae231a1c9a
Moises Stiker <stiker.moises@gmail.com>
2024-09-02 12:25:55 +0000

    Replace anarchy-desktop.nix

--- etc/nixos/anarchy-desktop.nix
+++ etc/nixos/anarchy-desktop.nix
@@ -6,11 +6,20 @@ environment.systemPackages = with pkgs; [
 	citrix_workspace
 	element-desktop
 	spotifywm
+	mgba
+	cemu
+	ryujinx
+	bsnes-hd
+	dolphin-emu
 	stoken
 	teams-for-linux
 	telegram-desktop
 ];
 nixpkgs.config.permittedInsecurePackages = [
 	"jitsi-meet-1.0.8043"
+	"electron-29.4.6"
 ];
+services.udev.extraRules = ''
+  ACTION=="add" SUBSYSTEM=="pci" ATTR{vendor}=="0x1022" ATTR{device}=="0x1484" ATTR{power/wakeup}="disabled"
+'';
 }
