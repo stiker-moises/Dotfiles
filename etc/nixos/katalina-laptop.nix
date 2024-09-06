934a8dff38ace42fa0a52d138bf351e971549a1c
Moises Stiker <stiker.moises@gmail.com>
2024-09-06 01:40:21 +0000

    Replace katalina-laptop.nix

--- etc/nixos/katalina-laptop.nix
+++ etc/nixos/katalina-laptop.nix
@@ -2,7 +2,12 @@
 {
 environment.systemPackages = with pkgs; [
 	chromium
+	element-desktop
+	spotifywm
+	telegram-desktop
 ];
 nixpkgs.config.permittedInsecurePackages = [
+	"jitsi-meet-1.0.8043"
+	"electron-29.4.6"
 ];
 }
