f8ceabde252c6791f4e0b96135b396495bf05ff5
Moises Stiker <stiker.moises@gmail.com>
2024-09-14 20:17:10 +0000

    Replace sys.nix

--- etc/nixos/sys.nix
+++ etc/nixos/sys.nix
@@ -138,9 +138,29 @@ fonts = {
 		};
 	};
 };
+
 security = {
 	rtkit.enable = true;
 	sudo.enable = false;
+	polkit = {
+		enable = true;
+		extraConfig = ''
+			polkit.addRule(function(action, subject) {
+				if (
+					subject.isInGroup("users")
+					&& (
+						action.id == "org.freedesktop.login1.reboot" ||
+						action.id == "org.freedesktop.login1.reboot-multiple-sessions" ||
+						action.id == "org.freedesktop.login1.power-off" ||
+						action.id == "org.freedesktop.login1.power-off-multiple-sessions"
+					)
+				)
+				{
+					return polkit.Result.YES;
+				}
+			});
+		'';
+	};
 	doas = {
 		enable = true;
 		extraRules = [{
@@ -149,6 +169,21 @@ security = {
 			persist = true;
 			noLog = true;
 		}];
+	};
+};
+systemd = {
+	user.services.polkit-gnome-authentication-agent-1 = {
+		description = "polkit-gnome-authentication-agent-1";
+		wantedBy = [ "graphical-session.target" ];
+		wants = [ "graphical-session.target" ];
+		after = [ "graphical-session.target" ];
+		serviceConfig = {
+			Type = "simple";
+			ExecStart = "${pkgs.polkit_gnome}/libexec/polkit-gnome-authentication-agent-1";
+			Restart = "on-failure";
+			RestartSec = 1;
+			TimeoutStopSec = 10;
+		};
 	};
 };
 services = {
